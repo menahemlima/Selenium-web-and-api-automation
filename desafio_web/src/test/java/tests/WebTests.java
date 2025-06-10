1b8c82084b736f1d14ae4a010578e5a541cfd541
menahemlima <menahemlima@gmail.com>
2025-06-10 15:23:21 -0300

    Update class name

--- desafio_web/src/test/java/tests/WebTests.java
+++ desafio_web/src/test/java/tests/WebTests.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class DesafioSicrediTests {
+public class WebTests {
     private WebDriver navegador;
 
     /* locators dos elementos */
